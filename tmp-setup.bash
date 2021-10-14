424b1f16d5bb9fc7a6b5d12d97e8a1f0dde750bb
TAKAHASHI Shuuji <shuuji3@gmail.com>
2021-10-14 21:18:03 +0900

    Add a temporary note required to set up the cluster

---
+++ tmp-setup.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Temporary note required to setup the cluster
+
+ssh-import-id gh:shuuji3
+
+sudo hostnamectl set-hostname pi-0
+
+# Setup docker
+sudo snap install docker
+sudo addgroup docker
+sudo adduser ubuntu docker
+sudo usermod -aG docker ubuntu
+sudo chown root:docker /var/run/docker.sock
+sudo chown -R root:docker /var/run/docker
+
+# Install arkade
+curl -sLS https://get.arkade.dev | sudo sh
+echo 'export PATH=$PATH:$HOME/.arkade/bin/' >> .bashrc
+. .bashrc
+
+# Install
+ark get gh kind kubectl jq yq argocd-autopilot kubescape kubens kubectx k3sup minikube
+
+# Following tools could not be installed for unknown reasons...
+# x 404 krew
+# x 403 terraform vagrant
+
+gh auth login
