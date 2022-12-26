6285d0c10634c57fa44ac597955e6e0ac4da74a3
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-12-26 21:42:21 +0900

    chore: delete cert-manager

--- install-cert-manager.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
-helm repo add cert-manager https://charts.jetstack.io
-helm install cert-manager cert-manager/cert-manager --version 1.8.0 -n cert-manager
